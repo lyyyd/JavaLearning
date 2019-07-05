a9b3c88873cc79e91eae053a0ed65ab453bc2656
OrangeManDavid <OrangeHeroDavid@163.com>
2019-07-06 07:38:14 +0800

    Demo12HundredSum.java

--- 01.Java基础语法/03.【 流程控制语句】/02_代码/Demo12HundredSum.java
+++ 01.Java基础语法/03.【 流程控制语句】/02_代码/Demo12HundredSum.java
@@ -9,7 +9,6 @@
 public class Demo12HundredSum {
 	public static void main(String[] args) {
 		int sum = 0; // 用来累加的存钱罐
-		
 		for (int i = 1; i <= 100; i++) {
 			if (i % 2 == 0) { // 如果是偶数
 				sum += i;
