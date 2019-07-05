a40d85dd4d0ff06a0b3ca7e5dcfa08dc75ac8396
OrangeManDavid <OrangeHeroDavid@163.com>
2019-07-06 06:51:01 +0800

    Demo02If.java

---
+++ 01.Java基础语法/03.【 流程控制语句】/02_代码/Demo02If.java
@@ -0,0 +1,13 @@
+// 单if语句
+public class Demo02If {
+	public static void main(String[] args) {
+		System.out.println("今天天气不错，正在压马路……突然发现一个快乐的地方：网吧");
+		int age = 19;
+		if (age >= 18) {
+			System.out.println("进入网吧，开始high！");
+			System.out.println("遇到了一群猪队友，开始骂街。");
+			System.out.println("感觉不爽，结账走人。");
+		}
+		System.out.println("回家吃饭");
+	}
+}
