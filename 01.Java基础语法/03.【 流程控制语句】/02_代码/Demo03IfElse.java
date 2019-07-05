f05406671cb43d3e0e85fd4e0e69412b0bc46e01
OrangeManDavid <OrangeHeroDavid@163.com>
2019-07-06 06:59:00 +0800

    Demo03IfElse.java

---
+++ 01.Java基础语法/03.【 流程控制语句】/02_代码/Demo03IfElse.java
@@ -0,0 +1,12 @@
+// 标准的if-else语句
+public class Demo03IfElse {
+	public static void main(String[] args) {
+		int num = 666;
+		
+		if (num % 2 == 0) { // 如果除以2能够余数为0，说明是偶数
+			System.out.println("偶数");
+		} else {
+			System.out.println("奇数");
+		}
+	}
+}
