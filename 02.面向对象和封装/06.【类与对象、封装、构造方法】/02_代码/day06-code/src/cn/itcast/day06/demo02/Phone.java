bb4428dd501ca4275ec8f1a53153482278356f70
OrangeManDavid <OrangeHeroDavid@163.com>
2019-07-07 21:37:24 +0800

    day06 demo02

--- 02.面向对象和封装/06.【类与对象、封装、构造方法】/02_代码/day06-code/src/cn/itcast/day06/demo02/Phone.java
+++ 02.面向对象和封装/06.【类与对象、封装、构造方法】/02_代码/day06-code/src/cn/itcast/day06/demo02/Phone.java
@@ -6,13 +6,13 @@ package cn.itcast.day06.demo02;
 行为：打电话、发短信
 
 对应到类当中：
-成员变量（属性）：
-    String brand; // 品牌
-    double price; // 价格
-    String color; // 颜色
-成员方法（行为）：
-    public void call(String who) {} // 打电话
-    public void sendMessage() {} // 群发短信
+    成员变量（属性）：
+        String brand; // 品牌
+        double price; // 价格
+        String color; // 颜色
+    成员方法（行为）：
+        public void call(String who) {} // 打电话
+        public void sendMessage() {} // 群发短信
  */
 public class Phone {
 
