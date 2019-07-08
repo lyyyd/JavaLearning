06656eab4c653ae0fbc2b1d4890d920b80a2ef00
OrangeManDavid <OrangeHeroDavid@163.com>
2019-07-08 20:06:27 +0800

    demo02

---
+++ 03.会员版(2.0)-就业课(2.0)-常用API第一部分/07.【Scanner类、Random类、ArrayList类】/02_代码/day07-code/src/cn/itcast/day07/demo02/Demo02Anonymous.java
@@ -0,0 +1,39 @@
+package cn.itcast.day07.demo02;
+
+import java.util.Scanner;
+
+public class Demo02Anonymous {
+
+    public static void main(String[] args) {
+        // 普通使用方式
+//        Scanner sc = new Scanner(System.in);
+//        int num = sc.nextInt();
+
+        // 匿名对象的方式
+//        int num = new Scanner(System.in).nextInt();
+//        System.out.println("输入的是：" + num);
+
+        // 使用一般写法传入参数
+//        Scanner sc = new Scanner(System.in);
+//        methodParam(sc);
+
+        // 使用匿名对象来进行传参
+//        methodParam(new Scanner(System.in));
+
+        Scanner sc = methodReturn();
+        int num = sc.nextInt();
+        System.out.println("输入的是：" + num);
+    }
+
+    public static void methodParam(Scanner sc) {
+        int num = sc.nextInt();
+        System.out.println("输入的是：" + num);
+    }
+
+    public static Scanner methodReturn() {
+//        Scanner sc = new Scanner(System.in);
+//        return sc;
+        return new Scanner(System.in);
+    }
+
+}
