33bfda0401469f9367b8c248a0a4ca409ee865ad
OrangeManDavid <OrangeHeroDavid@163.com>
2019-07-08 20:07:07 +0800

    demo02

---
+++ 03.会员版(2.0)-就业课(2.0)-常用API第一部分/07.【Scanner类、Random类、ArrayList类】/02_代码/day07-code/src/cn/itcast/day07/demo03/Demo02Random.java
@@ -0,0 +1,16 @@
+package cn.itcast.day07.demo03;
+
+import java.util.Random;
+
+public class Demo02Random {
+
+    public static void main(String[] args) {
+        Random r = new Random();
+
+        for (int i = 0; i < 100; i++) {
+            int num = r.nextInt(10); // 范围实际上是0~9
+            System.out.println(num);
+        }
+    }
+
+}
