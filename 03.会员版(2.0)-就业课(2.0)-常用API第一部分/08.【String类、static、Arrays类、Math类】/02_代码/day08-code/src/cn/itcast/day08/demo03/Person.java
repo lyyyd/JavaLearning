85986270f1bc7eaabaf594e6ae4365c670ee3058
OrangeManDavid <OrangeHeroDavid@163.com>
2019-07-09 20:47:07 +0800

    day08 demo03

---
+++ 03.会员版(2.0)-就业课(2.0)-常用API第一部分/08.【String类、static、Arrays类、Math类】/02_代码/day08-code/src/cn/itcast/day08/demo03/Person.java
@@ -0,0 +1,14 @@
+package cn.itcast.day08.demo03;
+
+
+public class Person {
+
+    static {
+        System.out.println("静态代码块执行！");
+    }
+
+    public Person() {
+        System.out.println("构造方法执行！");
+    }
+
+}
