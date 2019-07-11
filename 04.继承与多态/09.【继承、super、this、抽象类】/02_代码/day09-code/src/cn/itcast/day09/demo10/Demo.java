aef6a504838ca7db1e91a875ad2f1a7f7be14104
OrangeManDavid <OrangeHeroDavid@163.com>
2019-07-11 22:33:07 +0800

    day09 demo10

---
+++ 04.继承与多态/09.【继承、super、this、抽象类】/02_代码/day09-code/src/cn/itcast/day09/demo10/Demo.java
@@ -0,0 +1,12 @@
+package cn.itcast.day09.demo10;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        Zi zi = new Zi();
+
+        zi.show();
+        zi.method();
+    }
+
+}
