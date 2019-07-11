717869200a950fcd375a66eba1abd20e93bcd9bb
OrangeManDavid <OrangeHeroDavid@163.com>
2019-07-11 22:29:42 +0800

    day09 demo01

---
+++ 04.继承与多态/09.【继承、super、this、抽象类】/02_代码/day09-code/src/cn/itcast/day09/demo03/Zi.java
@@ -0,0 +1,14 @@
+package cn.itcast.day09.demo03;
+
+public class Zi extends Fu {
+
+    int num = 20;
+
+    public void method() {
+        int num = 30;
+        System.out.println(num); // 30，局部变量
+        System.out.println(this.num); // 20，本类的成员变量
+        System.out.println(super.num); // 10，父类的成员变量
+    }
+
+}
