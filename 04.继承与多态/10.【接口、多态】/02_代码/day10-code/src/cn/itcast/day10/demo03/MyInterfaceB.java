8bf6397a8431b0d254ae119bcae3b61e6474ca60
OrangeManDavid <OrangeHeroDavid@163.com>
2019-07-12 07:52:31 +0800

    day10

---
+++ 04.继承与多态/10.【接口、多态】/02_代码/day10-code/src/cn/itcast/day10/demo03/MyInterfaceB.java
@@ -0,0 +1,13 @@
+package cn.itcast.day10.demo03;
+
+public interface MyInterfaceB {
+
+    public abstract void methodB();
+
+    public abstract void methodCommon();
+
+    public default void methodDefault() {
+        System.out.println("BBB");
+    }
+
+}
