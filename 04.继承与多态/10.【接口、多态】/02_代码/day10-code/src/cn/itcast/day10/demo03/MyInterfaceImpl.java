46afc8be07285e4e48e1c04996f3c4ea74f5dfe3
OrangeManDavid <OrangeHeroDavid@163.com>
2019-07-12 07:52:35 +0800

    day10

---
+++ 04.继承与多态/10.【接口、多态】/02_代码/day10-code/src/cn/itcast/day10/demo03/MyInterfaceImpl.java
@@ -0,0 +1,23 @@
+package cn.itcast.day10.demo03;
+
+public class MyInterfaceImpl implements MyInterface {
+    @Override
+    public void method() {
+
+    }
+
+    @Override
+    public void methodA() {
+
+    }
+
+    @Override
+    public void methodB() {
+
+    }
+
+    @Override
+    public void methodCommon() {
+
+    }
+}
