c006732e024c12753dc45a708ddf7ce801af4ab7
OrangeManDavid <OrangeHeroDavid@163.com>
2019-07-11 22:30:18 +0800

    day09 demo01

---
+++ 04.继承与多态/09.【继承、super、this、抽象类】/02_代码/day09-code/src/cn/itcast/day09/demo04/Fu.java
@@ -0,0 +1,13 @@
+package cn.itcast.day09.demo04;
+
+public class Fu {
+
+    public void methodFu() {
+        System.out.println("父类方法执行！");
+    }
+
+    public void method() {
+        System.out.println("父类重名方法执行！");
+    }
+
+}
