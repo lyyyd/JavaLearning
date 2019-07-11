62c4c5ee09de96792f2102112431fbc5f82ab9e6
OrangeManDavid <OrangeHeroDavid@163.com>
2019-07-12 07:51:44 +0800

    day10

---
+++ 04.继承与多态/10.【接口、多态】/02_代码/day10-code/src/cn/itcast/day10/demo02/MyInterfaceAbstract.java
@@ -0,0 +1,20 @@
+package cn.itcast.day10.demo02;
+
+public abstract class MyInterfaceAbstract implements MyInterfaceA, MyInterfaceB {
+    @Override
+    public void methodA() {
+
+    }
+
+    @Override
+    public void methodAbs() {
+
+    }
+
+    @Override
+    public void methodDefault() {
+
+    }
+
+
+}
