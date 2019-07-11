45cf195bf2a06b0733d7b0b57ad3cd9d29dd5e0b
OrangeManDavid <OrangeHeroDavid@163.com>
2019-07-12 07:49:55 +0800

    day10

---
+++ 04.继承与多态/10.【接口、多态】/02_代码/day10-code/src/cn/itcast/day10/demo01/Demo04Interface.java
@@ -0,0 +1,12 @@
+package cn.itcast.day10.demo01;
+
+public class Demo04Interface {
+
+    public static void main(String[] args) {
+        MyInterfacePrivateB.methodStatic1();
+        MyInterfacePrivateB.methodStatic2();
+        // 错误写法！
+//        MyInterfacePrivateB.methodStaticCommon();
+    }
+
+}
