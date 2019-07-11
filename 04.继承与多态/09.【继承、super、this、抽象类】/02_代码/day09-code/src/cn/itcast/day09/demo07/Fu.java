c4b8b88ddad29326d2958eedfc4b70efa41c2a93
OrangeManDavid <OrangeHeroDavid@163.com>
2019-07-11 22:31:44 +0800

    day09 demo07

---
+++ 04.继承与多态/09.【继承、super、this、抽象类】/02_代码/day09-code/src/cn/itcast/day09/demo07/Fu.java
@@ -0,0 +1,13 @@
+package cn.itcast.day09.demo07;
+
+public class Fu {
+
+    public Fu() {
+        System.out.println("父类无参构造");
+    }
+
+    public Fu(int num) {
+        System.out.println("父类有参构造！");
+    }
+
+}
