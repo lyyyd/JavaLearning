18239246780b462e7e71a6b59bf9794bc115e55d
OrangeManDavid <OrangeHeroDavid@163.com>
2019-07-12 07:50:38 +0800

    day10

---
+++ 04.继承与多态/10.【接口、多态】/02_代码/day10-code/src/cn/itcast/day10/demo01/MyInterfaceDefaultA.java
@@ -0,0 +1,8 @@
+package cn.itcast.day10.demo01;
+
+public class MyInterfaceDefaultA implements MyInterfaceDefault {
+    @Override
+    public void methodAbs() {
+        System.out.println("实现了抽象方法，AAA");
+    }
+}
