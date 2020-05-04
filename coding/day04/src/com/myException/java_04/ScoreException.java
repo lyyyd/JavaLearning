0f7aa8f8b2a1ba7d8f0d26013f4919d6429bea4a
OrangeManDavid <david.jackson.lyd@gmail.com>
2020-05-04 21:41:01 +0800

    day04

---
+++ coding/day04/src/com/myException/java_04/ScoreException.java
@@ -0,0 +1,11 @@
+package com.myException.java_04;
+
+public class ScoreException extends Exception {
+
+    public ScoreException() {}
+
+    public ScoreException(String message) {
+        super(message);
+    }
+
+}
