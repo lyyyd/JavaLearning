810db4983751d125235c7fda06397dc4e37c2b4a
OrangeManDavid <david.jackson.lyd@gmail.com>
2020-05-05 20:16:21 +0800

    day11-12

---
+++ coding/day11/src/com/myThread/java_03/ThreadPriority.java
@@ -0,0 +1,12 @@
+package com.myThread.java_03;
+
+public class ThreadPriority extends Thread {
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(getName() + ":" + i);
+        }
+    }
+
+}
