47002136ee21147217b0364e991b7d89be151ca0
OrangeManDavid <david.jackson.lyd@gmail.com>
2020-06-22 22:03:47 +0800

    springboot-helloworld

---
+++ 05.流行框架/springboot/springboot-helloworld/src/main/java/com/itheima/HelloApplication.java
@@ -0,0 +1,15 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 引导类。 SpringBoot项目的入口
+ */
+@SpringBootApplication
+public class HelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApplication.class,args);
+    }
+}
