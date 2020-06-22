2c77c6923d5f9f6abb79c999e3e30429c536a02c
OrangeManDavid <david.jackson.lyd@gmail.com>
2020-06-22 22:20:42 +0800

    springboot-profiles

---
+++ 05.流行框架/springboot/springboot-profiles/src/main/java/com/itheima/springbootconfiginner/SpringbootConfigInnerApplication.java
@@ -0,0 +1,13 @@
+package com.itheima.springbootconfiginner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootConfigInnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootConfigInnerApplication.class, args);
+    }
+
+}
---
+++ 05.流行框架/springboot/springboot-profiles/src/test/java/com/itheima/springbootconfiginner/SpringbootConfigInnerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.itheima.springbootconfiginner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootConfigInnerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
