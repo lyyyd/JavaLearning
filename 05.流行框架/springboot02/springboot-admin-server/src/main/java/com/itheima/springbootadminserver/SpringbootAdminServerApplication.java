c15f54d776a3584cc31aeccd54beb8bdc4dfe94a
OrangeManDavid <david.jackson.lyd@gmail.com>
2020-06-25 08:32:55 +0800

    springboot-admin-server

---
+++ 05.流行框架/springboot02/springboot-admin-server/src/main/java/com/itheima/springbootadminserver/SpringbootAdminServerApplication.java
@@ -0,0 +1,15 @@
+package com.itheima.springbootadminserver;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAdminServer
+@SpringBootApplication
+public class SpringbootAdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAdminServerApplication.class, args);
+    }
+
+}
