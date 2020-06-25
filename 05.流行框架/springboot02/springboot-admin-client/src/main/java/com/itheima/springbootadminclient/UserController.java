9a7d6173d1bbec780406b8bbeb206d171eea8d3e
OrangeManDavid <david.jackson.lyd@gmail.com>
2020-06-25 08:32:36 +0800

    springboot-admin-client

---
+++ 05.流行框架/springboot02/springboot-admin-client/src/main/java/com/itheima/springbootadminclient/UserController.java
@@ -0,0 +1,15 @@
+package com.itheima.springbootadminclient;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+
+    @RequestMapping("/findAll")
+    public String findAll(){
+        return "success";
+    }
+}
